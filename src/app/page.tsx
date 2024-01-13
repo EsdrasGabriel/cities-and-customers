2a8da6e3ddf607b6c0d2d8d3d0cc2ab908d31ccf
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2024-01-13 11:49:33 -0300

    feat: added search and filter  component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import FilterModal from "@/components/FilterModal";
+import Search from "@/components/Search";
+
 export default function Home() {
   return (
-      <section className="flex h-full">
-        <main className="flex-1">
-          Hello World
-        </main>
-      </section>
+      <main className="flex-1 p-10 h-full flex flex-col items-start justify-start">
+        <section className="flex items-center justify-between w-full">
+          <h1 className="text-4xl font-semibold text-zinc-800">Listing of Customers and Cities</h1>
+          <div className="flex gap-5">
+            <FilterModal />
+            <Search />
+          </div>
+        </section>
+        <section>Hello</section>
+      </main>
   )
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import { FiSearch } from "react-icons/fi";
+
+export default function Search() {
+    return (
+            <form action="">
+                <div className="relative flex items-center justify-start">
+                    <FiSearch className="absolute text-xl text-red-400 ml-2"/>
+                    <input type="text" className="bg-transparent border border-zinc-300 placeholder:text-zinc-400 py-2 pr-2 pl-8 rounded-lg w-96" placeholder="Search"/>
+                </div>
+            </form>
+    )
+}
